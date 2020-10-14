90658be3ad35b869ff6c9f002aa5aa8220801e83
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-14 22:14:44 +0200

    Created FavouriteList component & modidified the CurrencyList component,also added styles to the currencyList

--- src/Components/CurrencyList/CurrencyList.jsx
+++ src/Components/CurrencyList/CurrencyList.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+ import FavouriteList from '../FavouriteList/FavouriteList'
+ import './CurrencyList.scss';
 
 
-
-const CurrencyList = () => {
-  return (
-    <div className="homepage">
-      <h3>CurrencyList</h3>
-    </div>
-  );
-};
-
+class CurrencyList extends React.Component {
+   
+  
+    render() {
+      return (
+        <React.Fragment>
+          <div>
+            <h3 className="title">Favourite Currency List </h3>
+          </div>
+  
+          <FavouriteList />
+        </React.Fragment>
+      );
+    }
+  }
+  
 export default CurrencyList;
