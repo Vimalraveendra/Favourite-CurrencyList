22449492436036b7bfd9e97925f5eb9cf392bdd2
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-14 15:00:10 +0200

    Created Page Folder and Added HomePage , also created components folder & added CurrencyList Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import HomePage from './Page/HomePage/HomePage';
 import './App.css';
 
 const App=()=> {
   return (
     <div className="App">
       <header className="App-header">
-       <h3>Favourite CurrencyList</h3>
+       <HomePage/>
       </header>
     </div>
   );
